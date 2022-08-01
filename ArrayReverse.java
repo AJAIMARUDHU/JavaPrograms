5e99589c7ae41a4704b5b47cfa97eb05543b88cd
AJAIMARUDHU <94695780+AJAIMARUDHU@users.noreply.github.com>
2022-08-01 19:37:16 +0530

    Update ArrayReverse.java

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -20,9 +20,8 @@ public class ArrayReverse {
         arr[i]=arr[n];
         arr[n]=temp;
     }
-    System.out.println(Arrays.toString(arr));
     for(int i=0;i<arr.length;i++){
-        System.out.println(arr[i]);
+        System.out.println(arr[i]+" ");
     }
     }
 }
